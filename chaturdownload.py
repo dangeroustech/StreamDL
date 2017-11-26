457b6385bd63d2be6b5d1000aab09ad442e4b977
Josh Jacobs <josh@joshjacobs.net>
2017-11-26 18:24:05 +0000

    Stream acquired

--- chaturdownload.py
+++ chaturdownload.py
@@ -3,6 +3,9 @@
 import logging
 import argparse
 import sys
+import re
+from bs4 import BeautifulSoup as bs
+import requests
 
 LOG_LEVEL = logging.DEBUG
 FORMAT = '%(asctime)s %(levelname)s: %(message)s'
@@ -11,7 +14,7 @@ FORMAT = '%(asctime)s %(levelname)s: %(message)s'
 def main(argv):
     parser = argparse.ArgumentParser(usage='Download the Current Stream by a Chaturbate User')
     parser.add_argument('-u', '--user', help='Chaturbate Username', required=True)
-    parser.add_argument('-l', '--logfile', help='Logfile to use (defaults to working dir)')
+    parser.add_argument('-l', '--logpath', help='Logfile to use (defaults to working dir)')
 
     args = parser.parse_args()
 
@@ -20,16 +23,35 @@ def main(argv):
     else:
         logfile = args.logpath
     user = args.user
+
     logging.basicConfig(filename=logfile, level=LOG_LEVEL, format=FORMAT)
     logging.debug("Starting ChaturDownload...")
     logging.debug("Using User: {}".format(user))
 
+    stream, title = get_stream(user)
+    logging.debug("Stream URL Received: {}", stream)
+    logging.debug("Stream Title Received: {}", title)
+
 
 def get_stream(user):
-    logging.debug("Getting Stream...")
+    logging.debug("FUNCTION: Getting Stream...")
+    regex = re.compile('https:..edge.+?m3u8')
+    URL = "https://chaturbate.com/{}".format(user)
+    r = requests.get(URL)
+    soup = bs(r.text, 'html.parser')
+    stream = str(soup.find_all(string=regex))
+    stream_url = re.search(regex, stream).group()
+    room_title = ""
+
+    for string in soup.strings:
+        if "default_subject" in string:
+            room_title = re.search('default_subject: \"(.+)\"', string).group().lstrip("default_subject: \"").rstrip("\"")
+
+    return stream_url, room_title.replace("%20", "_").replace("%23", "#")
 
 
-def download_video(stream):
-    logging.debug("Downloading Video...")
+def download_video(stream, filename):
+    logging.debug("FUNCTION: Downloading Video...")
+
 
 main(sys.argv)
