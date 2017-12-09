1c587d12f76d2bceb43da756e25def8a74f2394f
Josh Jacobs <josh@joshjacobs.net>
2017-12-09 14:38:15 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+
+setup(
+    name="ChaturDownload",
+    version="1.0",
+    description="Good shits",
+    author="Pat Riarchy",
+    packages=["ChaturDownload"],
+    install_requires=['bs4', 'requests', 'ffmpeg-python'],
+)
