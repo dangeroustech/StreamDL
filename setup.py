6d6890381aa672f4b512b9eb41034f9ef1987867
Josh Jacobs <josh@joshjacobs.net>
2019-10-07 22:33:16 +0100

    Setup.py update
    - Version bump
    - Hard locked package versions

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 
 setup(
     name="CBDL",
-    version="2.0",
-    description="Good shits",
+    version="2.1",
+    description="Download Streams From Your Favourite Nefarious Website",
     author="Pat Riarchy",
-    install_requires=['youtube_dl', 'pyYAML'],
+    install_requires=['youtube_dl==2019.09.28', 'PyYAML==5.1.2'],
 )
