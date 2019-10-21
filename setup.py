56e262c257df9622a2723a545fe250b6cfb62642
Josh Jacobs <josh@joshjacobs.net>
2019-10-21 21:16:53 +0100

    Match setup.py to pipenv and bump minor version

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 
 setup(
     name="StreamDL",
-    version="2.3",
+    version="2.3.1",
     description="Monitor and Download Streams from a Variety of Websites",
     author="biodrone",
-    install_requires=['youtube_dl==2019.09.28', 'PyYAML==5.1.2'],
+    install_requires=['youtube_dl==2019.09.28', 'PyYAML==5.1.2', 'requests==2.22'],
 )
