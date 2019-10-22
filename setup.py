a0edd6ba0e4a1aef4ac41909ed28baed8db4542a
Josh Jacobs <josh@joshjacobs.net>
2019-10-22 17:02:57 +0100

    Move setup.py to v1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="StreamDL",
-    version="2.3.1",
+    version="1.0.0",
     description="Monitor and Download Streams from a Variety of Websites",
     author="biodrone",
     install_requires=['youtube_dl==2019.09.28', 'PyYAML==5.1.2', 'requests==2.22'],
