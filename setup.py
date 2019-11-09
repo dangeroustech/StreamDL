d1f86daab5050a884d528d975e713c93958a5f5b
Josh Jacobs <josh@joshjacobs.net>
2019-11-09 20:57:49 +0000

    Youtube-dl version bump

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     version="1.0.0",
     description="Monitor and Download Streams from a Variety of Websites",
     author="biodrone",
-    install_requires=['youtube_dl==2019.09.28', 'PyYAML==5.1.2', 'requests==2.22'],
+    install_requires=['youtube_dl==2019.11.05', 'PyYAML==5.1.2', 'requests==2.22'],
 )
