a3350834f3ff8e3767ac64794c65ea4ce1492d17
Josh Jacobs <josh@joshjacobs.net>
2019-11-21 19:58:13 +0000

    StreamDL version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="StreamDL",
-    version="1.2.1",
+    version="1.2.2",
     description="Monitor and Download Streams from a Variety of Websites",
     author="biodrone",
     install_requires=['youtube_dl==2019.11.22', 'PyYAML==5.1.2', 'requests==2.22'],
