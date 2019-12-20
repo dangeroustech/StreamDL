31e87c55c9d2a69344d19fb5fd2b516e86bbcc69
Josh Jacobs <josh@joshjacobs.net>
2019-12-20 01:20:09 +0000

    Hardcode Org Name

--- travis/dockerhub-deploy.sh
+++ travis/dockerhub-deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ev
 export DOCKER_CLI_EXPERIMENTAL=enabled
-DOCKERHUB_ORG=dangerous-tech
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     DOCKER_TAG="stable"
 else
@@ -11,27 +10,27 @@ fi
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
     # Push stable manifest
-    docker manifest create ${DOCKERHUB_ORG}/streamdl:${DOCKER_TAG} \
-            ${DOCKERHUB_ORG}/streamdl:${DOCKER_TAG}-amd64 \
-            ${DOCKERHUB_ORG}/streamdl:${DOCKER_TAG}-arm \
-            ${DOCKERHUB_ORG}/streamdl:${DOCKER_TAG}-arm64
+    docker manifest create dangerous-tech/streamdl:${DOCKER_TAG} \
+            dangerous-tech/streamdl:${DOCKER_TAG}-amd64 \
+            dangerous-tech/streamdl:${DOCKER_TAG}-arm \
+            dangerous-tech/streamdl:${DOCKER_TAG}-arm64
 
-    docker manifest annotate ${DOCKERHUB_ORG}/streamdl:${DOCKER_TAG} ${DOCKERHUB_ORG}/streamdl:${DOCKER_TAG}-amd64 --arch amd64
-    docker manifest annotate ${DOCKERHUB_ORG}/streamdl:${DOCKER_TAG} ${DOCKERHUB_ORG}/streamdl:${DOCKER_TAG}-arm --arch arm
-    docker manifest annotate ${DOCKERHUB_ORG}/streamdl:${DOCKER_TAG} ${DOCKERHUB_ORG}/streamdl:${DOCKER_TAG}-arm64 --arch arm64
+    docker manifest annotate dangerous-tech/streamdl:${DOCKER_TAG} dangerous-tech/streamdl:${DOCKER_TAG}-amd64 --arch amd64
+    docker manifest annotate dangerous-tech/streamdl:${DOCKER_TAG} dangerous-tech/streamdl:${DOCKER_TAG}-arm --arch arm
+    docker manifest annotate dangerous-tech/streamdl:${DOCKER_TAG} dangerous-tech/streamdl:${DOCKER_TAG}-arm64 --arch arm64
 
-    docker manifest push ${DOCKERHUB_ORG}/streamdl:${DOCKER_TAG}
+    docker manifest push dangerous-tech/streamdl:${DOCKER_TAG}
     # Push latest manifest
-    docker manifest create ${DOCKERHUB_ORG}/streamdl:latest \
-            ${DOCKERHUB_ORG}/streamdl:latest-amd64 \
-            ${DOCKERHUB_ORG}/streamdl:latest-arm \
-            ${DOCKERHUB_ORG}/streamdl:latest-arm64
+    docker manifest create dangerous-tech/streamdl:latest \
+            dangerous-tech/streamdl:latest-amd64 \
+            dangerous-tech/streamdl:latest-arm \
+            dangerous-tech/streamdl:latest-arm64
 
-    docker manifest annotate ${DOCKERHUB_ORG}/streamdl:latest ${DOCKERHUB_ORG}/streamdl:latest-amd64 --arch amd64
-    docker manifest annotate ${DOCKERHUB_ORG}/streamdl:latest ${DOCKERHUB_ORG}/streamdl:latest-arm --arch arm
-    docker manifest annotate ${DOCKERHUB_ORG}/streamdl:latest ${DOCKERHUB_ORG}/streamdl:latest-arm64 --arch arm64
+    docker manifest annotate dangerous-tech/streamdl:latest dangerous-tech/streamdl:latest-amd64 --arch amd64
+    docker manifest annotate dangerous-tech/streamdl:latest dangerous-tech/streamdl:latest-arm --arch arm
+    docker manifest annotate dangerous-tech/streamdl:latest dangerous-tech/streamdl:latest-arm64 --arch arm64
 
-    docker manifest push ${DOCKERHUB_ORG}/streamdl:latest
+    docker manifest push dangerous-tech/streamdl:latest
 else
     docker images
 fi
