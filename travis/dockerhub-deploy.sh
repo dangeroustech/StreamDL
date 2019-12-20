dff9dcad394548a2d52204c2b5064eed6f7e2f16
Josh Jacobs <josh@joshjacobs.net>
2019-12-20 09:15:58 +0000

    Correct Org Name

--- travis/dockerhub-deploy.sh
+++ travis/dockerhub-deploy.sh
@@ -10,27 +10,27 @@ fi
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
     # Push stable manifest
-    docker manifest create dangerous-tech/streamdl:${DOCKER_TAG} \
-            dangerous-tech/streamdl:${DOCKER_TAG}-amd64 \
-            dangerous-tech/streamdl:${DOCKER_TAG}-arm \
-            dangerous-tech/streamdl:${DOCKER_TAG}-arm64
+    docker manifest create dangeroustech/streamdl:${DOCKER_TAG} \
+            dangeroustech/streamdl:${DOCKER_TAG}-amd64 \
+            dangeroustech/streamdl:${DOCKER_TAG}-arm \
+            dangeroustech/streamdl:${DOCKER_TAG}-arm64
 
-    docker manifest annotate dangerous-tech/streamdl:${DOCKER_TAG} dangerous-tech/streamdl:${DOCKER_TAG}-amd64 --arch amd64
-    docker manifest annotate dangerous-tech/streamdl:${DOCKER_TAG} dangerous-tech/streamdl:${DOCKER_TAG}-arm --arch arm
-    docker manifest annotate dangerous-tech/streamdl:${DOCKER_TAG} dangerous-tech/streamdl:${DOCKER_TAG}-arm64 --arch arm64
+    docker manifest annotate dangeroustech/streamdl:${DOCKER_TAG} dangeroustech/streamdl:${DOCKER_TAG}-amd64 --arch amd64
+    docker manifest annotate dangeroustech/streamdl:${DOCKER_TAG} dangeroustech/streamdl:${DOCKER_TAG}-arm --arch arm
+    docker manifest annotate dangeroustech/streamdl:${DOCKER_TAG} dangeroustech/streamdl:${DOCKER_TAG}-arm64 --arch arm64
 
-    docker manifest push dangerous-tech/streamdl:${DOCKER_TAG}
+    docker manifest push dangeroustech/streamdl:${DOCKER_TAG}
     # Push latest manifest
-    docker manifest create dangerous-tech/streamdl:latest \
-            dangerous-tech/streamdl:latest-amd64 \
-            dangerous-tech/streamdl:latest-arm \
-            dangerous-tech/streamdl:latest-arm64
+    docker manifest create dangeroustech/streamdl:latest \
+            dangeroustech/streamdl:latest-amd64 \
+            dangeroustech/streamdl:latest-arm \
+            dangeroustech/streamdl:latest-arm64
 
-    docker manifest annotate dangerous-tech/streamdl:latest dangerous-tech/streamdl:latest-amd64 --arch amd64
-    docker manifest annotate dangerous-tech/streamdl:latest dangerous-tech/streamdl:latest-arm --arch arm
-    docker manifest annotate dangerous-tech/streamdl:latest dangerous-tech/streamdl:latest-arm64 --arch arm64
+    docker manifest annotate dangeroustech/streamdl:latest dangeroustech/streamdl:latest-amd64 --arch amd64
+    docker manifest annotate dangeroustech/streamdl:latest dangeroustech/streamdl:latest-arm --arch arm
+    docker manifest annotate dangeroustech/streamdl:latest dangeroustech/streamdl:latest-arm64 --arch arm64
 
-    docker manifest push dangerous-tech/streamdl:latest
+    docker manifest push dangeroustech/streamdl:latest
 else
     docker images
 fi
