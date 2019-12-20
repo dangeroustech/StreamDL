61285d3a15eefca8840ed7ebe14b86d4a0c56c70
Josh Jacobs <josh@joshjacobs.net>
2019-12-20 12:55:49 +0000

    Tidy Up Comments

--- travis/build-amd64.sh
+++ travis/build-amd64.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -ev
 
+# Set Build Options
 PLATFORM=amd64
 DOCKERFILE_LOCATION="./Dockerfile"
 DOCKER_IMAGE="streamdl"
 DOCKER_ORG="dangeroustech"
+
+# Set Correct Branch
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     DOCKER_TAG="stable"
 else
     DOCKER_TAG="staging"
 fi
 
+# If This Isn't A PR, Push to Dockerhub
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 
@@ -32,6 +36,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
         --local dockerfile=. \
         --local context=.
 else
+    # If This is a PR, Build to Check for Errors
     buildctl build --frontend dockerfile.v0 \
         --progress=plain \
         --opt platform=linux/${PLATFORM} \
--- travis/dockerhub-deploy.sh
+++ travis/dockerhub-deploy.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+
 set -ev
 export DOCKER_CLI_EXPERIMENTAL=enabled
+
+# Set Correct Branch
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     DOCKER_TAG="stable"
 else
     DOCKER_TAG="staging"
 fi
 
+# If This Isn't A PR, Push to Dockerhub
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-    # Push stable manifest
+
     docker manifest create dangeroustech/streamdl:${DOCKER_TAG} \
             dangeroustech/streamdl:${DOCKER_TAG}-amd64 \
             dangeroustech/streamdl:${DOCKER_TAG}-arm \
@@ -20,7 +24,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     docker manifest annotate dangeroustech/streamdl:${DOCKER_TAG} dangeroustech/streamdl:${DOCKER_TAG}-arm64 --arch arm64
 
     docker manifest push dangeroustech/streamdl:${DOCKER_TAG}
-    # Push latest manifest
+
     docker manifest create dangeroustech/streamdl:latest \
             dangeroustech/streamdl:latest-amd64 \
             dangeroustech/streamdl:latest-arm \
@@ -32,5 +36,6 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 
     docker manifest push dangeroustech/streamdl:latest
 else
+    # If This is a PR, Check Images and Tags Are Correct
     docker images
 fi
