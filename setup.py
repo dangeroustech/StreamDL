65ef0fc079ef50a39b3c308e92eacd443452b71f
Josh Jacobs <josh@joshjacobs.net>
2020-04-16 20:45:28 +0100

    Update Python to 3.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="StreamDL",
-    version="1.2.2",
+    version="1.2.3",
     description="Monitor and Download Streams from a Variety of Websites",
     author="biodrone",
     install_requires=['youtube_dl==2020.3.24', 'PyYAML==5.3.1', 'requests==2.23.0'],
