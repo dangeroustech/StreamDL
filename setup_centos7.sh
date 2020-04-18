61f7966e0eff691bdd99037f1ddf08015f378bb0
Josh Jacobs <josh@joshjacobs.net>
2020-04-18 16:51:57 +0100

    Activate Correct SCL Module

--- setup_centos7.sh
+++ setup_centos7.sh
@@ -5,6 +5,6 @@ sudo yum install centos-release-scl -y
 sudo yum update -y
 sudo yum install ffmpeg rh-python38 -y
 sudo yum groupinstall 'Development Tools' -y
-scl enable rh-python36 bash
+scl enable rh-python38 bash
 sudo pip install pipenv
 pipenv install -e .
