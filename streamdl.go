775ac3265837852e359dd201a96ded49052d2922
Josh Jacobs <josh@datasolace.io>
2024-03-26 21:11:31 +0000

    refactor: gofmt

--- streamdl.go
+++ streamdl.go
@@ -4,10 +4,10 @@ import (
 	"flag"
 	"os"
 	"os/signal"
+	"sort"
+	"strings"
 	"syscall"
 	"time"
-	"strings"
-	"sort"
 
 	log "github.com/sirupsen/logrus"
 	prefixed "github.com/x-cray/logrus-prefixed-formatter"
